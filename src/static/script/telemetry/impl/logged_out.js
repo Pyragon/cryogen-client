01f6ce165212f38a08e39b91d93b7ac36f227fe0
Pyragon <eldo.imo.rs@hotmail.com>
2018-07-30 03:27:48 -0700

    Fix focusing on window on logout telemetry event

--- src/static/script/telemetry/impl/logged_out.js
+++ src/static/script/telemetry/impl/logged_out.js
@@ -9,16 +9,12 @@ var _logged = () => {
     receive: (data) => {
       sendNotification('Logged Out Event', 'You have logged out @ ' + dateFormat(new Date(), 'h:MM:ss TT'), () => {
         if (data && data.pid) {
-          windows.getProcess((error, processes) => {
+          windows.getProcesses((error, processes) => {
             if (error) {
               console.error(error);
               return;
             }
-            processes.forEach(console.log);
-            var clients = processes.filter(p => {
-              console.log(p.pid);
-              return p.pid == data.pid;
-            });
+            var clients = processes.filter(p => p.pid == data.pid);
             if (clients.length > 0)
               windows.focusWindow(clients[0]);
           });
